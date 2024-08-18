fc12262552506906c247a7dc1768e70d06403052
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-18 18:11:00 +0600

    private route page added

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -3,6 +3,7 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../Providers/AuthProvider";
 
 const PrivateRoute = ({children}) => {
+    
     const {user, loading} = useContext(AuthContext);
     if (loading) {
         return <span className="loading loading-infinity loading-lg"></span>
